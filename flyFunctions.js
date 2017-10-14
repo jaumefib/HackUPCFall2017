23090c218d9af16b26577adf64fbf4138fc027e7
jaumefib <jaume.pladevall@est.fib.upc>
2017-10-14 17:02:48 +0200

    acabada comanda fly

--- flyFunctions.js
+++ flyFunctions.js
@@ -1,30 +1,56 @@
+
+var S = require('string');
+// prova de les funcions de fly
 detect_commands("I want to fly from Barcelona to Madrid");
+
+// Variables globals
+var country = "";
+var currency = "";
+var locale = "";
+var originPlace = "";
+var destinationPlace = "";
+var outboundPartialDate = "";
+var inboundPartialDate = "";
+
+
+
+
+
+
+
 
 // funcio gestionar missatge
 
 function detect_commands(q) {
-  q.toLowerCase();
-  if (q.includes("fly") || q.includes("flight")) comanda_fly(q.text);
+  S(q).toLowerCase();
+  if (S(q).contains("fly") || S(q).includes("flight")) comanda_fly(q);
 
 
 
-  else if (q.includes("hello") || q.includes("Hello")) comanda_hello();
+  else if (S(q).contains('hello') || S(q).includes("Hello")) comanda_hello();
 }
 
-function comanda_fly(q) {  // from to
-  if (q.includes("from")) {
-    var originId = "a";
-    var destinationId = "b";
-    var i;
-    for (i = q.indexOf("from")+5; i > 0 && q[i] != ' ' && i < q.length; ++i) {
-      originId = originId + q[i].text;
+function comanda_fly(q) {
+  // si la comanda conte la paraula from
+  var original = q;
+  if (S(q).indexOf("from") != -1) {
+    var i, j;
+    i =  S(q).indexOf("from")+5;
+    j = i;
+    for (i; i > 0 && q[i] != ' ' && i < S(q).length; ++i) {
+      originPlace = originPlace + q[i];
     }
-    for (i = q.indexOf("to")+3; i > 0 && q[i] != ' ' && i < q.length; ++i) {
-      destinationId = destinationId + q[i].text;
+    var tmp = "";
+    for (j; j < S(q).length; ++j) tmp = tmp + q[j];
+    q = tmp;
+    for (i = S(q).indexOf("to")+3; i > 0 && q[i] != ' ' && i < S(q).length; ++i) {
+      destinationPlace = destinationPlace + q[i];
     }
-    if (originId.length == 0) originId = "everywhere";
-    if (destinationId.length == 0) destinationId = "everywhere";
   }
-  console.log(originId);
-  console.log(destinationId);
+  if (originPlace.length == 0) originPlace = "everywhere";
+  if (destinationPlace.length == 0) destinationPlace = "everywhere";
+  console.log(originPlace);
+  console.log(destinationPlace);
+
+  // ara crida a la funcio de GET de Skyscanner
 }
