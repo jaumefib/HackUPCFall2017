59db5cc16566d28b1132f5cfb5f961678331e6f2
jaumefib <jaume.pladevall@est.fib.upc>
2017-10-14 18:00:49 +0200

    acabant command fly i exemples

--- flyFunctions.js
+++ flyFunctions.js
@@ -2,6 +2,7 @@
 var S = require('string');
 // prova de les funcions de fly
 detect_commands("I want to fly from Barcelona to Madrid");
+detect_commands("How are you doing");
 
 // Variables globals
 var country = "";
@@ -24,13 +25,13 @@ var inboundPartialDate = "";
 function detect_commands(q) {
   S(q).toLowerCase();
   if (S(q).contains("fly") || S(q).includes("flight")) comanda_fly(q);
-
-
-
-  else if (S(q).contains('hello') || S(q).includes("Hello")) comanda_hello();
+  //else if (S(q).contains("car") || S(q).includes("driver")) comanda_car(q);
+  else if (S(q).contains('hello') ||  S(q).includes("hi") || S(q).includes("help") ) comanda_hello();
 }
 
 function comanda_fly(q) {
+  originPlace = "";
+  destinationPlace = "";
   // si la comanda conte la paraula from
   var original = q;
   if (S(q).indexOf("from") != -1) {
@@ -49,8 +50,42 @@ function comanda_fly(q) {
   }
   if (originPlace.length == 0) originPlace = "everywhere";
   if (destinationPlace.length == 0) destinationPlace = "everywhere";
-  console.log(originPlace);
-  console.log(destinationPlace);
+  console.log("Origin " + originPlace);
+  console.log("Destination " + destinationPlace);
+
+  // assignem la comanda original a q, que ha estat modificada
+  q = tmp;
+
+  inboundPartialDate = "";
+  outboundPartialDate = "";
+  if (S(q).indexOf("in") != -1) {
+    i =  S(q).indexOf("in")+3;
+    j = i;
+    for (i; i > 0 && q[i] != ' ' && i < S(q).length; ++i) {
+      outboundPartialDate = outboundPartialDate + q[i];
+    }
+    var tmp = "";
+    for (j; j < S(q).length; ++j) tmp = tmp + q[j];
+    q = tmp;
+    for (i = S(q).indexOf("in")+3; i > 0 && q[i] != ' ' && i < S(q).length; ++i) {
+      inboundPartialDate = inboundPartialDate + q[i];
+    }
+  }
+
+  if (outboundPartialDate.length == 0) {
+      console.log("Departure anytime");
+      if (inboundPartialDate.length != 0) {
+        console.log("Arrival " + inboundPartialDate);
+      }
+  }
+  else console.log("Departure " + outboundPartialDate);
+
+  if (inboundPartialDate.length != 0) {
+    console.log("Arrival " + inboundPartialDate);
+  }
+
+    // falta la opcio nomes de anada
+
 
   // ara crida a la funcio de GET de Skyscanner
 }
