7c35adb82b9c0c2f824a14f646d07a0c039b6625
jaumefib <jaume.pladevall@est.fib.upc>
2017-10-14 15:27:10 +0200

    funcions comandes fly

--- flyFunctions.js
+++ flyFunctions.js
@@ -1,125 +1,30 @@
-var express = require('express');
-var app = express();
+detect_commands("I want to fly from Barcelona to Madrid");
 
-app.set('port', (process.env.PORT || 5000));
+// funcio gestionar missatge
 
-app.use(express.static(__dirname + '/public'));
+function detect_commands(q) {
+  q.toLowerCase();
+  if (q.includes("fly") || q.includes("flight")) comanda_fly(q.text);
 
-// Views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
-// Verify
-app.get('/webhook', function(req, res) {
-  res.status(200).send(req.query['hub.challenge']);
-});
+  else if (q.includes("hello") || q.includes("Hello")) comanda_hello();
+}
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-//Post
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
+function comanda_fly(q) {  // from to
+  if (q.includes("from")) {
+    var originId = "a";
+    var destinationId = "b";
+    var i;
+    for (i = q.indexOf("from")+5; i > 0 && q[i] != ' ' && i < q.length; ++i) {
+      originId = originId + q[i].text;
+    }
+    for (i = q.indexOf("to")+3; i > 0 && q[i] != ' ' && i < q.length; ++i) {
+      destinationId = destinationId + q[i].text;
+    }
+    if (originId.length == 0) originId = "everywhere";
+    if (destinationId.length == 0) destinationId = "everywhere";
   }
-});
-  
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Hello");
-  }
-}
-
-function sendGenericMessage(recipientId, messageText) {
-  // To be expanded in later sections
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAERQwLFnEwBALjWcZBqXZBdoaYXYFt9cSvwDxXGb4oARA3eUZAFy3jUAOlXetCaG3GmCnrnfWCWxe98iZCil5ZAuSjcsrMua9NazIjX7996s0z5NdGZBbLFfOQABeVNDYUfvua1YR2V0pAR9K4LVtZAZCrOfQ1B6icCfr8ZAjhDDj0HasK5f8244' },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
+  console.log(originId);
+  console.log(destinationId);
 }
